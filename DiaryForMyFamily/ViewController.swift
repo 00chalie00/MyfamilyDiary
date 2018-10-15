f6bc75a153bb6de10bd8d002b543890c38fcb3bc
Lee Chul <winfe79@hanmail.net>
2018-10-15 17:06:39 +0900

    Initial Commit

---
+++ DiaryForMyFamily/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiaryForMyFamily
+//
+//  Created by formathead on 2018. 10. 15..
+//  Copyright © 2018년 formathead. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ DiaryForMyFamilyTests/DiaryForMyFamilyTests.swift
@@ -0,0 +1,34 @@
+//
+//  DiaryForMyFamilyTests.swift
+//  DiaryForMyFamilyTests
+//
+//  Created by formathead on 2018. 10. 15..
+//  Copyright © 2018년 formathead. All rights reserved.
+//
+
+import XCTest
+@testable import DiaryForMyFamily
+
+class DiaryForMyFamilyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
