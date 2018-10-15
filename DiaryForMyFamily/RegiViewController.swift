59b24b6de1a1b57104b461ba3db5e67eff8d55ef
Lee Chul <winfe79@hanmail.net>
2018-10-15 18:03:38 +0900

    The View Move Complete

---
+++ DiaryForMyFamily/RegiViewController.swift
@@ -0,0 +1,9 @@
+//
+//  RegiViewController.swift
+//  DiaryForMyFamily
+//
+//  Created by formathead on 2018. 10. 15..
+//  Copyright © 2018년 formathead. All rights reserved.
+//
+
+import Foundation
---
+++ DiaryForMyFamily/TableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TableViewController.swift
+//  DiaryForMyFamily
+//
+//  Created by formathead on 2018. 10. 15..
+//  Copyright © 2018년 formathead. All rights reserved.
+//
+
+import Foundation
